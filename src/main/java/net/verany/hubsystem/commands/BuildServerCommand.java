3bd7bcf260f9771b2c612442ca6ad4f6a69576dd
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-13 23:09:05 +0100

    Fixed build server command
    Fixed build server command:
    - fixed sound
    - fixed sendOnServer-functionality

--- src/main/java/net/verany/hubsystem/commands/BuildServerCommand.java
+++ src/main/java/net/verany/hubsystem/commands/BuildServerCommand.java
@@ -2,6 +2,7 @@ package net.verany.hubsystem.commands;
 
 import net.verany.api.Verany;
 import net.verany.api.player.IPlayerInfo;
+import net.verany.api.prefix.PrefixPattern;
 import net.verany.hubsystem.HubSystem;
 import org.bukkit.Sound;
 import org.bukkit.command.Command;
@@ -18,10 +19,14 @@ public class BuildServerCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         Player player = (Player) sender;
         IPlayerInfo playerInfo = Verany.PROFILE_OBJECT.getPlayer(player.getUniqueId()).get();
+
+        playerInfo.playRiptideAnimation();
+
         if(player.hasPermission("verany.hub.buildserver")) {
             if(args.length == 0) {
                 player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "§7Du wirst zum BauServer gesendet§8...");
-                player.playSound(player.getLocation(), Sound.MUSIC_CREDITS, 2F, 2F);
+                player.playSound(player.getLocation(), Sound.ENTITY_PLAYER_LEVELUP, 2F, 2F);
+                playerInfo.sendOnServer("Build-1");
             } else {
                 player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "§cDeine Eingabe ist leider falsch gewesen§8.");
                 }
