255fd9efc847629549d28ce590f5df3c5af8e55e
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-15 23:44:15 +0100

    Updated JoinEvent
    Updated JoinEvent:
    - Outsourcing of hotbar items during join
    - Added setting to teleport to last position on login

--- src/main/java/net/verany/hubsystem/events/JoinEvent.java
+++ src/main/java/net/verany/hubsystem/events/JoinEvent.java
@@ -4,6 +4,9 @@ import net.verany.api.Verany;
 import net.verany.api.itembuilder.ItemBuilder;
 import net.verany.api.player.IPlayerInfo;
 import net.verany.api.skull.SkullBuilder;
+import net.verany.hubsystem.HubSystem;
+import net.verany.hubsystem.utils.player.HubPlayer;
+import net.verany.hubsystem.utils.settings.HubSetting;
 import org.bukkit.Bukkit;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
@@ -19,30 +22,30 @@ public class JoinEvent implements Listener {
     @EventHandler
     public void onJoin(PlayerJoinEvent event) {
         event.setJoinMessage(null);
-        Location spawnLocation = new Location(Bukkit.getWorld("world"), 6, 67, 13);
         Player player = event.getPlayer();
+        IPlayerInfo playerInfo = Verany.PROFILE_OBJECT.getPlayer(player.getUniqueId()).get();
 
         player.getInventory().clear();
         player.getInventory().setArmorContents(null);
         player.setExp(0);
         player.setGameMode(GameMode.ADVENTURE);
-        player.teleport(spawnLocation);
+
+        HubPlayer hubPlayer = new HubPlayer(HubSystem.INSTANCE);
+        hubPlayer.load(player.getUniqueId());
+        Verany.setPlayer(HubPlayer.class, hubPlayer);
+
+        if (playerInfo.getSettingValue(HubSetting.LAST_LOCATION_TELEPORT))
+            player.teleport(hubPlayer.getData(HubPlayer.PlayerData.class).getLastLocation().toLocation());
+        else
+            player.teleport(HubSystem.INSTANCE.getLocationManager().getLocation("spawn"));
+
+        hubPlayer.setItems();
+
         player.setFoodLevel(20);
         player.setHealth(20);
         player.setHealthScale(2);
         player.setFlying(false);
         player.setLevel(0);
-
-        IPlayerInfo playerInfo = Verany.PROFILE_OBJECT.getPlayer(player.getUniqueId()).get();
-
-        player.getInventory().setItem(0, new ItemBuilder(Material.FIREWORK_ROCKET).setAmount(1).setDisplayName("§8◗§7◗ §b§lTeleporter").build());
-        player.getInventory().setItem(1, new ItemBuilder(Material.COMPASS).setAmount(1).setDisplayName("§8◗§7◗ §b§lLoot Compass").build());
-        player.getInventory().setItem(2, new ItemBuilder(Material.NAME_TAG).setAmount(1).setDisplayName("§8◗§7◗ §b§lNick").build());
-        player.getInventory().setItem(4, new ItemBuilder(Material.TRIDENT).setAmount(1).setDisplayName("§8◗§7◗ §b§lTrident").build());
-        player.getInventory().setItem(6, new ItemBuilder(Material.BOOK).setAmount(1).setDisplayName("§8◗§7◗ §b§lInbox").build());
-        player.getInventory().setItem(7, new ItemBuilder(Material.CLOCK).setAmount(1).setDisplayName("§8◗§7◗ §b§lHub Switcher").build());
-        player.getInventory().setItem(8, new SkullBuilder(playerInfo.getSkinData()).setDisplayName("§8◗§7◗ §b§lProfile").build());
-        //player.getInventory().setItem(8, new ItemBuilder(Material.COMPARATOR).setAmount(1).setDisplayName("§8◗§7◗ §b§lProfile").build());
 
     }
 }
--- src/main/java/net/verany/hubsystem/events/QuitEvent.java
+++ src/main/java/net/verany/hubsystem/events/QuitEvent.java
@@ -1,6 +1,10 @@
 package net.verany.hubsystem.events;
 
+import net.verany.api.Verany;
+import net.verany.hubsystem.utils.location.HubLocation;
+import net.verany.hubsystem.utils.player.HubPlayer;
 import org.bukkit.OfflinePlayer;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
@@ -9,6 +13,13 @@ public class QuitEvent implements Listener {
 
     @EventHandler
     public void onQuit(PlayerQuitEvent event) {
+        Player player = event.getPlayer();
+
         event.setQuitMessage(null);
+
+        HubPlayer hubPlayer =Verany.getPlayer(player.getUniqueId().toString(), HubPlayer.class);
+        hubPlayer.getData(HubPlayer.PlayerData.class).setLastLocation(HubLocation.toHubLocation(player.getLocation()));
+        hubPlayer.update();
+        Verany.removePlayer(player.getUniqueId().toString(), HubPlayer.class);
     }
 }
