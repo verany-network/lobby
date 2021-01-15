632feaf8b38715d1c6f59ba9c2307466973ba7b9
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-15 23:42:10 +0100

    Updated setup command message

--- src/main/java/net/verany/hubsystem/commands/SetupCommand.java
+++ src/main/java/net/verany/hubsystem/commands/SetupCommand.java
@@ -18,33 +18,35 @@ public class SetupCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
         Player player = (Player) sender;
         IPlayerInfo playerInfo = Verany.PROFILE_OBJECT.getPlayer(player.getUniqueId()).get();
-        if(args.length == 0) {
-            if(player.hasPermission("verany.hub.setup")) {
-                player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "§7Setup");
-                player.sendMessage("§a");
-                player.sendMessage(" §8× §b/setup §8• §7Zeigt diese Liste");
-                player.sendMessage(" §8× §b/setup build §8• §7Bringt dich in den Baumodus");
-                player.sendMessage(" §8× §b/setup addbees §8• §7Fügt ein Bienen-Nest hinzu");
-                player.sendMessage(" §8× §b/setup setnpc [NPC] §8• §7Setzt einen NPC");
-                player.sendMessage(" §8× §b/setup setspawn §8• §7Setzt den Spawnpunkt");
-                player.sendMessage(" §8× §b/setup setloc [Location] §8• §7Setzt eine Location");
-                player.sendMessage("§b");
-            } else {
-                player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "§cDazu hast du keine Berechtigung§8.");
-            }
-        } else if(args.length == 1) {
+
+        if (!player.hasPermission("verany.hub.setup")) {
+            player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "§cDazu hast du keine Berechtigung§8.");
+            return false;
+        }
+
+        if (args.length == 0) {
+            player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "§7Setup");
+            player.sendMessage("§a");
+            player.sendMessage(" §8× §b/setup §8• §7Zeigt diese Liste");
+            player.sendMessage(" §8× §b/setup build §8• §7Bringt dich in den Baumodus");
+            player.sendMessage(" §8× §b/setup addbees §8• §7Fügt ein Bienen-Nest hinzu");
+            player.sendMessage(" §8× §b/setup setnpc [NPC] §8• §7Setzt einen NPC");
+            player.sendMessage(" §8× §b/setup setspawn §8• §7Setzt den Spawnpunkt");
+            player.sendMessage(" §8× §b/setup setloc [Location] §8• §7Setzt eine Location");
+            player.sendMessage("§b");
+        } else if (args.length == 1) {
             String name = args[0];
             switch (name.toLowerCase()) {
                 case "setspawn":
                     HubSystem.INSTANCE.getLocationManager().createLocation("spawn", player.getLocation());
-                    player.sendMessage("spawn ist gesetzt bro");
+                    player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "Der §bSpawn §7wurde gesetzt§8.");
                     break;
             }
-        } else if(args.length == 2) {
+        } else if (args.length == 2) {
             if (args[0].equalsIgnoreCase("setloc")) {
                 String name = args[1];
                 HubSystem.INSTANCE.getLocationManager().createLocation(name, player.getLocation());
-                player.sendMessage(name + " ist gesetzt bro");
+                player.sendMessage(playerInfo.getPrefix(HubSystem.INSTANCE.getModule()) + "Die Location §b" + name + " §7wurde gesetzt§.");
             }
         }
         return false;
