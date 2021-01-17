04c88f4cfaeb7ab002b4c5621f62f96c392f700a
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-17 01:38:14 +0100

    Added ingame config and set bee boolean  to false

---
+++ src/main/java/net/verany/hubsystem/utils/config/HubConfig.java
@@ -0,0 +1,9 @@
+package net.verany.hubsystem.utils.config;
+
+import net.verany.api.config.IngameConfig;
+
+public class HubConfig {
+
+    public static final IngameConfig<Boolean> BEES_SPAWNED = new IngameConfig<>(Boolean.class, false);
+
+}
