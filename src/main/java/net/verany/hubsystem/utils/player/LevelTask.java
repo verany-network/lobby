0cf830b6fd651fbb0d168c05e612e2420a8c7cbf
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-17 02:17:01 +0100

    Added level task to display user count in level bar

---
+++ src/main/java/net/verany/hubsystem/utils/player/LevelTask.java
@@ -0,0 +1,21 @@
+package net.verany.hubsystem.utils.player;
+
+import de.dytanic.cloudnet.ext.bridge.BridgePlayerManager;
+import net.verany.api.task.AbstractTask;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class LevelTask extends AbstractTask {
+    public LevelTask(long waitTime) {
+        super(waitTime);
+    }
+
+    @Override
+    public void run() {
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            int online = BridgePlayerManager.getInstance().getOnlineCount();
+            onlinePlayer.setLevel(online);
+            onlinePlayer.setExp((float) online / 512);
+        }
+    }
+}
