2f153b2ef78f3da6d67adae49f22d41a14741922
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-17 01:37:01 +0100

    Added comment

--- src/main/java/net/verany/hubsystem/events/QuitEvent.java
+++ src/main/java/net/verany/hubsystem/events/QuitEvent.java
@@ -1,8 +1,10 @@
 package net.verany.hubsystem.events;
 
 import net.verany.api.Verany;
+import net.verany.hubsystem.utils.config.HubConfig;
 import net.verany.hubsystem.utils.location.HubLocation;
 import net.verany.hubsystem.utils.player.HubPlayer;
+import org.bukkit.Bukkit;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -21,5 +23,15 @@ public class QuitEvent implements Listener {
         hubPlayer.getData(HubPlayer.PlayerData.class).setLastLocation(HubLocation.toHubLocation(player.getLocation()));
         hubPlayer.update();
         Verany.removePlayer(player.getUniqueId().toString(), HubPlayer.class);
+
+
+        /* if(!HubConfig.BEES_SPAWNED.getValue()) {
+            if(Bukkit.getOnlinePlayers().size()) {
+
+            }
+        } */
+
     }
+
+
 }
