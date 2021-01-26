4f252f64f8089dc81589954800b29ea94c5770b9
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-26 20:08:02 +0100

    Removed actionbar error message and added error sound. And Fixed trident

--- src/main/java/net/verany/hubsystem/events/InteractEvent.java
+++ src/main/java/net/verany/hubsystem/events/InteractEvent.java
@@ -1,16 +1,10 @@
 package net.verany.hubsystem.events;
 
 import net.verany.api.Verany;
-import net.verany.api.actionbar.DefaultActionbar;
-import net.verany.api.inventory.InventoryBuilder;
 import net.verany.api.player.IPlayerInfo;
-import net.verany.hubsystem.utils.inventories.NewTeleporterInventory;
 import net.verany.hubsystem.utils.inventories.NickInventory;
 import net.verany.hubsystem.utils.inventories.TeleporterInventory;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.World;
+import org.bukkit.*;
 import org.bukkit.block.Block;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -28,45 +22,45 @@ public class InteractEvent implements Listener {
         Player player = event.getPlayer();
 
 
-        if(event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
+        if (event.getAction().equals(Action.RIGHT_CLICK_BLOCK) || event.getAction().equals(Action.RIGHT_CLICK_AIR)) {
             IPlayerInfo playerInfo = Verany.PROFILE_OBJECT.getPlayer(player.getUniqueId()).get();
             switch (player.getInventory().getItemInMainHand().getType()) {
 
                 case FIREWORK_ROCKET:
-                    new NewTeleporterInventory(player).setItems(NewTeleporterInventory.TeleporterCategory.GAMES);
+                    new TeleporterInventory(player).setItems(TeleporterInventory.TeleporterCategory.GAMES);
                     break;
 
                 case COMPASS:
-                    playerInfo.addActionbar(new DefaultActionbar("§cDerzeit gibt es keinen Loot auf der Hub§8.", 1000));
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_BREAK, 2F, 2F);
                     break;
 
                 case NAME_TAG:
-                    playerInfo.addActionbar(new DefaultActionbar("§cDas Nick Tool ist derzeit deaktiviert§8.", 1000));
+                    new NickInventory(player);
                     break;
 
                 case TRIDENT:
-                    playerInfo.addActionbar(new DefaultActionbar("§cDer Trident ist derzeit deaktiviert§8.", 1000));
+                    event.setCancelled(event.getClickedBlock() != null && !event.getClickedBlock().isLiquid());
                     break;
 
                 case BOOK:
-                    playerInfo.addActionbar(new DefaultActionbar("§cDie Inbox ist derzeit deaktiviert§8.", 1000));
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_BREAK, 2F, 2F);
                     break;
 
                 case CLOCK:
-                    playerInfo.addActionbar(new DefaultActionbar("§cDer Hub Switcher ist derzeit deaktiviert§8.", 1000));
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_BREAK, 2F, 2F);
                     break;
 
                 case PLAYER_HEAD:
-                    playerInfo.addActionbar(new DefaultActionbar("§cDas Profil ist derzeit deaktiviert§8.", 1000));
+                    player.playSound(player.getLocation(), Sound.BLOCK_ANVIL_BREAK, 2F, 2F);
                     break;
             }
 
         }
 
         Block targetBlock = player.getTargetBlock(5);
-        if(targetBlock.getType().equals(Material.END_PORTAL_FRAME)) {
-            if(targetBlock.getLocation().getBlockX() == 47) {
-                if(targetBlock.getLocation().getBlockZ() == 24) {
+        if (targetBlock.getType().equals(Material.END_PORTAL_FRAME)) {
+            if (targetBlock.getLocation().getBlockX() == 47) {
+                if (targetBlock.getLocation().getBlockZ() == 24) {
                     player.sendMessage("jo war die richtige location");
                     World world = player.getWorld();
                     Location lootBoxLocation = new Location(Bukkit.getWorld("world"), 47.5, 62, 24.5);
