51340ea80ae4caa5bddfc999c2e86c94660b2c1c
Nico Kempe <50241630+nicokempe@users.noreply.github.com>
2021-01-31 01:12:06 +0100

    Updated Main and Profile inventory

--- src/main/java/net/verany/hubsystem/HubSystem.java
+++ src/main/java/net/verany/hubsystem/HubSystem.java
@@ -1,5 +1,6 @@
 package net.verany.hubsystem;
 
+import lombok.Getter;
 import net.verany.api.Verany;
 import net.verany.api.config.IngameConfig;
 import net.verany.api.inventory.InventoryBuilder;
@@ -9,13 +10,23 @@ import net.verany.hubsystem.commands.BuildServerCommand;
 import net.verany.hubsystem.commands.SetupCommand;
 import net.verany.hubsystem.commands.ToggleRankCommand;
 import net.verany.hubsystem.events.*;
+import net.verany.hubsystem.utils.bees.BeeTimeTask;
+import net.verany.hubsystem.utils.location.LocationManager;
+import net.verany.hubsystem.utils.player.LevelTask;
+import net.verany.hubsystem.utils.scoreboard.ScoreboardTask;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Bee;
 import org.bukkit.plugin.java.JavaPlugin;
 
-@VeranyModule(name = "HubSystem", prefix = "HubSystem", version = "1.1", authors = {"NicoVRNY", "tylix"}, user = "NicoVRNY", host = "159.69.63.105", password = "password", databases = {"players_hub"})
+import java.util.concurrent.TimeUnit;
+
+@Getter
+@VeranyModule(name = "HubSystem", prefix = "HubSystem", version = "1.1", authors = {"NicoVRNY", "tylix"}, user = "NicoVRNY", host = "159.69.63.105", password = "8Vu0T5MFd9KGTE1t", databases = {"hubsystem"})
 public class HubSystem extends VeranyProject {
 
     public static HubSystem INSTANCE;
+
+    private LocationManager locationManager;
 
     public HubSystem() {
         INSTANCE = this;
@@ -26,8 +37,12 @@ public class HubSystem extends VeranyProject {
         Verany.loadModule(this);
 
         IngameConfig.PLAYER_COLLISION.setValue(false);
+        IngameConfig.TAB_LIST.setValue(true);
         IngameConfig.TAB_LIST_FORMAT.setValue("{0}{1} §8▏ §7"); // 0 ist die Farbe, 1 ist der Rang und zwei ist
-        IngameConfig.CHAT_FORMAT.setValue("§8◗§7◗ {0}{1} §8▏ §7{2} §8• §f{3}"); // 0 ist die Farbe, 1 ist der Rang, 2 ist der Name und 3 ist die Nachricht
+        IngameConfig.TAB_LIST_CLAN.setValue(true);
+        IngameConfig.CHAT_FORMAT.setValue(" §8◗§7◗ {0}{1} §8▏ §7{2} §8• §f{3}"); // 0 ist die Farbe, 1 ist der Rang, 2 ist der Name und 3 ist die Nachricht
+        IngameConfig.AFK_TIME.setValue(TimeUnit.MINUTES.toMillis(10));
+        IngameConfig.AFK.setValue(true);
         // §8◗§7◗ §b§lVerany§3§lCloud §8▏ §7
 
         init();
@@ -38,6 +53,17 @@ public class HubSystem extends VeranyProject {
         super.init();
         registerCommands();
         registerEvents();
+
+        locationManager = new LocationManager(this);
+
+        Bukkit.getScheduler().runTaskTimer(this, new BeeTimeTask(), 0, 15);
+
+        Verany.addTask(new LevelTask(10 * 1000), new ScoreboardTask(1000), new ScoreboardTask.ScoreboardDisplayNameTask(100));
+    }
+
+    @Override
+    public void onDisable() {
+        locationManager.save();
     }
 
     private void registerCommands() {
--- src/main/java/net/verany/hubsystem/utils/inventories/ProfileInventory.java
+++ src/main/java/net/verany/hubsystem/utils/inventories/ProfileInventory.java
@@ -1,4 +1,94 @@
 package net.verany.hubsystem.utils.inventories;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import net.verany.api.Verany;
+import net.verany.api.enumhelper.VeranyEnum;
+import net.verany.api.hotbar.HotbarItem;
+import net.verany.api.itembuilder.ItemBuilder;
+import net.verany.api.player.IPlayerInfo;
+import net.verany.api.skull.SkullBuilder;
+import net.verany.hubsystem.HubSystem;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.ItemStack;
+
 public class ProfileInventory {
+
+    private final Player player;
+    private final IPlayerInfo playerInfo;
+    private final Integer[] profileCategorySlots = {19, 20, 21, 22, 23, 24, 25};
+
+    public ProfileInventory(Player player) {
+        this.player = player;
+        this.playerInfo = Verany.PROFILE_OBJECT.getPlayer(player.getUniqueId()).get();
+    }
+
+    public void setCategoryItems() {
+        for (int i = 0; i < ProfileCategory.values().length; i++) {
+            ProfileCategory category = ProfileCategory.values()[i];
+            playerInfo.setItem(profileCategorySlots[i], new HotbarItem(new ItemBuilder(category.equals(ProfileCategory.FRIENDS) ? new SkullBuilder(playerInfo.getSkinData()).build() : new ItemStack(category.getMaterial())).setGlow(player.getMetadata("profile.category").get(0).value().equals(category)), player) {
+                @Override
+                public void onClick(InventoryClickEvent event) {
+                    setItems(category).setCategoryItems();
+                }
+            });
+        }
+    }
+
+    public ProfileInventory setItems(ProfileCategory category) {
+        HubSystem.INSTANCE.setMetadata(player, "profile.category", category);
+
+        switch (category) {
+            case FRIENDS:
+                setFriendItems();
+                break;
+            case SETTINGS:
+                setSettingItems();
+                break;
+        }
+
+        return this;
+    }
+
+    private void setFriendItems() {
+
+    }
+
+    private void setSettingItems() {
+
+    }
+
+    @AllArgsConstructor
+    @Getter
+    public enum ProfileCategory implements VeranyEnum {
+        FRIENDS(Material.PLAYER_HEAD),
+        CLAN(Material.IRON_CHESTPLATE),
+        PARTY(Material.CAKE),
+        SETTINGS(Material.COMPARATOR),
+        MUSIC_AND_SOUNDS(Material.BELL),
+        INVENTORY(Material.ENDER_CHEST),
+        INBOX(Material.WRITABLE_BOOK);
+
+        private final Material material;
+
+        @AllArgsConstructor
+        @Getter
+        public enum SettingCategory implements VeranyEnum {
+            SOUND(Material.MUSIC_DISC_CAT),
+            PARTICLE(Material.BLAZE_POWDER),
+            ANIMATIONS(Material.ENCHANTING_TABLE),
+            FRIENDS(Material.PLAYER_HEAD),
+            CLANS(Material.IRON_CHESTPLATE),
+            PARTY(Material.CAKE),
+            PREFIX(Material.PAPER),
+            HUB(Material.BEACON),
+            VERIFICATIONS(Material.NAME_TAG),
+            ;
+
+            private final Material material;
+        }
+    }
+
 }
