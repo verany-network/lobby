303fd5e0d76377e44efbd02c6cd1b68c28d8930e
tylix <bytydoo@gmail.com>
2021-02-12 23:47:27 +0100

    Update HubConfig.java

--- src/main/java/net/verany/hubsystem/utils/config/HubConfig.java
+++ src/main/java/net/verany/hubsystem/utils/config/HubConfig.java
@@ -5,5 +5,6 @@ import net.verany.api.config.IngameConfig;
 public class HubConfig {
 
     public static final IngameConfig<Boolean> BEES_SPAWNED = new IngameConfig<>(Boolean.class, false);
+    public static final IngameConfig<Integer> ACTIONBAR_SECONDS = new IngameConfig<>(Integer.class, 7);
 
 }
