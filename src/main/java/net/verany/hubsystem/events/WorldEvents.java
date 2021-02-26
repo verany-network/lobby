e4613662f5ef92eaeeca301f23df065353ed43a7
tylix <bytydoo@gmail.com>
2021-02-26 12:55:14 +0100

    Update WorldEvents.java

--- src/main/java/net/verany/hubsystem/events/WorldEvents.java
+++ src/main/java/net/verany/hubsystem/events/WorldEvents.java
@@ -180,6 +180,8 @@ public class WorldEvents implements Listener {
             HubSystem.INSTANCE.removeMetadata(player, "hub_switcher");
         } else if (player.hasMetadata("teleporter")) {
             HubSystem.INSTANCE.removeMetadata(player, "teleporter");
+        } else if (player.hasMetadata("arcade")) {
+            HubSystem.INSTANCE.removeMetadata(player, "arcade");
         }
     }
 
