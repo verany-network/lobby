00ba075bac5f7ba8c0bd5fe56e226e1a9100ae29
tylix <bytydoo@gmail.com>
2021-04-15 23:58:42 +0200

    Update HubSwitcherInventory.java

--- hubsystem-parent/src/main/java/net/verany/hubsystem/game/inventory/HubSwitcherInventory.java
+++ hubsystem-parent/src/main/java/net/verany/hubsystem/game/inventory/HubSwitcherInventory.java
@@ -10,7 +10,9 @@ import net.verany.api.itembuilder.ItemBuilder;
 import net.verany.api.placeholder.Placeholder;
 import net.verany.api.player.IPlayerInfo;
 import net.verany.hubsystem.HubSystem;
+import org.bukkit.ChatColor;
 import org.bukkit.Material;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemFlag;
@@ -19,7 +21,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
-public class HubSwitcherInventory {
+public class HubSwitcherInventory implements IHubInventory {
 
     private final Player player;
     private final IPlayerInfo playerInfo;
@@ -30,23 +32,20 @@ public class HubSwitcherInventory {
         this.player = player;
         this.playerInfo = Verany.PROFILE_OBJECT.getPlayer(player.getUniqueId()).get();
         this.inventory = InventoryBuilder.builder().size(9 * 3).title(playerInfo.getKey("hub.switcher.title")).onClick(event -> {
-            String taskName = event.getCurrentItem().getType().equals(Material.GOLDEN_HELMET) ? "VIP-" : "" + "Hub";
-            List<ServiceInfoSnapshot> services = getSorted(taskName);
-            for (int i = 0; i < slots.length; i++) {
-                int slot = slots[i];
-                if (event.getSlot() == slot) {
-                    ServiceInfoSnapshot hub = services.get(i);
-                    if (taskName.startsWith("VIP") && !player.hasPermission("verany.vip")) continue;
-                    playerInfo.sendOnServer(hub.getName());
-                }
-            }
+            if (!event.getCurrentItem().getType().equals(Material.GOLDEN_HELMET) && !event.getCurrentItem().getType().equals(Material.IRON_HELMET))
+                return;
+            String serverName = ChatColor.stripColor(event.getCurrentItem().getItemMeta().getDisplayName().split(" ")[1]);
+            if (serverName.startsWith("VIP") && !player.hasPermission("verany.vip")) return;
+            playerInfo.playSound(Sound.ENTITY_PLAYER_LEVELUP, 1, 1.8F);
+            playerInfo.sendOnServer(serverName);
         }).build().fillCycle(new ItemBuilder(Material.valueOf(Verany.toDyeColor(playerInfo.getPrefixPattern().getColor().getFirstColor()) + "_STAINED_GLASS_PANE")).setNoName().build()).createAndOpen(player);
     }
 
+    @Override
     public void setItems() {
-        HubSystem.INSTANCE.setMetadata(player, "hub_switcher", this);
+        HubSystem.INSTANCE.setMetadata(player, "inventory", this);
 
-        String[] hubs = {"Hub", "VIPHub"};
+        String[] hubs = {"VIPHub", "Hub"};
 
         List<ServiceInfoSnapshot> services = new ArrayList<>();
         for (String task : hubs)
@@ -58,6 +57,9 @@ public class HubSwitcherInventory {
             int online = Verany.GAME_MODE_OBJECT.getOnlinePlayers(new String[]{hub.getServiceId().getTaskName()}, new String[]{name});
             inventory.setItem(slots[i], new ItemBuilder(hub.getServiceId().getName().startsWith("VIP") ? Material.GOLDEN_HELMET : Material.IRON_HELMET).setGlow(name.equals(playerInfo.getServer())).addItemFlag(ItemFlag.values()).setDisplayName(playerInfo.getKey("hub.switcher.item.hub", new Placeholder("%server_name%", name))).addLoreArray(playerInfo.getKeyArray("hub.switcher.item.hub.lore", '~', new Placeholder("%online%", Verany.asDecimal(online)))).build());
         }
+        for (int i = services.size(); i < slots.length; i++) {
+            inventory.setItem(slots[i], null);
+        }
 
     }
 
@@ -66,7 +68,8 @@ public class HubSwitcherInventory {
         Collection<ServiceInfoSnapshot> services = CloudNetDriver.getInstance().getCloudServiceProvider().getCloudServicesAsync(task).get();
         List<Verany.SortData<ServiceInfoSnapshot>> sortData = new ArrayList<>();
         for (ServiceInfoSnapshot service : services)
-            sortData.add(new Verany.SortData<>(service.getServiceId().getName(), service));
+            if (service.getProperty(BridgeServiceProperty.IS_ONLINE).isPresent() && service.getProperty(BridgeServiceProperty.IS_ONLINE).get())
+                sortData.add(new Verany.SortData<>(service.getServiceId().getName(), service));
         services = Verany.sortList(sortData, false);
         return new ArrayList<>(services);
     }
