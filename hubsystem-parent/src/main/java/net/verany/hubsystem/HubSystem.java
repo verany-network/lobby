403407a1f248063836616265c674ba5dd04e829a
tylix <bytydoo@gmail.com>
2021-04-15 23:58:51 +0200

    Update HubSystem.java

--- hubsystem-parent/src/main/java/net/verany/hubsystem/HubSystem.java
+++ hubsystem-parent/src/main/java/net/verany/hubsystem/HubSystem.java
@@ -9,13 +9,18 @@ import net.verany.api.module.VeranyModule;
 import net.verany.api.module.VeranyProject;
 import net.verany.hubsystem.commands.BuildCommand;
 import net.verany.hubsystem.commands.SetupCommand;
+import net.verany.hubsystem.game.HubManager;
+import net.verany.hubsystem.game.IHubManager;
 import net.verany.hubsystem.game.actionbar.ActionbarTask;
 import net.verany.hubsystem.game.bossbar.BossBarTask;
+import net.verany.hubsystem.game.inventory.task.InventoryTask;
 import net.verany.hubsystem.game.level.LevelTask;
+import net.verany.hubsystem.game.orb.OrbTask;
 import net.verany.hubsystem.game.scoreboard.ScoreboardTask;
 import net.verany.hubsystem.listener.PlayerJoinListener;
 import net.verany.hubsystem.listener.PlayerQuitListener;
 import net.verany.hubsystem.listener.ProtectionListener;
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Boss;
 
 import java.util.concurrent.TimeUnit;
@@ -25,7 +30,7 @@ import java.util.concurrent.TimeUnit;
         name = "HubSystem",
         prefix = "HubSystem",
         version = "1.1",
-        authors = {"NicoVRNY", "tylix"},
+        authors = {"tylix"},
         user = "NicoVRNY",
         host = "159.69.63.105",
         password = "8Vu0T5MFd9KGTE1t",
@@ -36,6 +41,7 @@ public class HubSystem extends VeranyProject {
     public static HubSystem INSTANCE;
 
     private AbstractLocationManager locationManager;
+    private IHubManager hubManager;
 
     public int playersInSupport = 0;
 
@@ -63,8 +69,10 @@ public class HubSystem extends VeranyProject {
         initCommands();
 
         locationManager = new LocationManager(this, "locations2", "hubsystem");
+        hubManager = new HubManager();
+        Bukkit.getScheduler().runTaskLater(this, () -> hubManager.registerArmorStands(), 20);
 
-        Verany.addTask(new ActionbarTask(750), new LevelTask(10 * 1000), new BossBarTask(50), new ScoreboardTask(1000), new ScoreboardTask.ScoreboardDisplayNameTask(150));
+        Verany.addTask(new ActionbarTask(750), new InventoryTask(1000), new OrbTask(50),new LevelTask(10 * 1000), new BossBarTask(50), new ScoreboardTask(1000), new ScoreboardTask.ScoreboardDisplayNameTask(150));
 
         IngameConfig.PLAYER_COLLISION.setValue(false);
         IngameConfig.TAB_LIST.setValue(true);
