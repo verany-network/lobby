3c00d8ea86df47d05895bcab3ad6f400f607ddb1
tylix <bytydoo@gmail.com>
2021-06-13 13:42:44 +0200

    Update PlayerJoinListener.java

--- hubsystem-parent/src/main/java/net/verany/hubsystem/listener/PlayerJoinListener.java
+++ hubsystem-parent/src/main/java/net/verany/hubsystem/listener/PlayerJoinListener.java
@@ -3,8 +3,12 @@ package net.verany.hubsystem.listener;
 import net.verany.api.Verany;
 import net.verany.api.event.AbstractListener;
 import net.verany.api.event.events.PlayerLoadCompleteEvent;
+import net.verany.api.locationmanager.AbstractLocationManager;
 import net.verany.api.module.VeranyProject;
+import net.verany.api.npc.INPC;
 import net.verany.api.player.IPlayerInfo;
+import net.verany.api.setup.AbstractSetupObject;
+import net.verany.api.setup.category.AbstractSetupCategory;
 import net.verany.hubsystem.HubSystem;
 import net.verany.hubsystem.game.scoreboard.HubScoreboard;
 import net.verany.hubsystem.game.scoreboard.IHubScoreboard;
@@ -40,15 +44,28 @@ public class PlayerJoinListener extends AbstractListener {
             scoreboard.load();
             hubPlayer.setScoreboard(scoreboard);
 
-            /*if (playerInfo.getSettingValue(HubSetting.LAST_LOCATION_TELEPORT))
+            if (playerInfo.getSettingValue(HubSetting.LAST_LOCATION_TELEPORT))
                 player.teleport(hubPlayer.getLastLocation().toLocation());
-            else*/
+            else
                 player.teleport(HubSystem.INSTANCE.getLocationManager().getLocation("spawn"));
 
             player.setGameMode(GameMode.ADVENTURE);
             player.setFoodLevel(20);
             player.setAllowFlight(false);
-            Bukkit.getScheduler().runTaskLater(HubSystem.INSTANCE, () -> player.setHealthScale(2), 2);
+            Bukkit.getScheduler().runTaskLater(HubSystem.INSTANCE, () -> {
+                player.setHealthScale(2);
+
+                HubSystem.INSTANCE.getLocationManager().getDataOptional(AbstractLocationManager.VeranyLocations.class).ifPresent(veranyLocations -> {
+                    veranyLocations.getLocations().forEach((s, veranyLocation) -> {
+                        if (s.startsWith("npc_")) {
+                            player.sendMessage("spawn npc");
+                            INPC npc = Verany.createNPC(s.split("_")[1], veranyLocation.toLocation(), true, player);
+                            npc.setGameProfile(playerInfo.getSkinData());
+                            npc.spawn();
+                        }
+                    });
+                });
+            }, 2);
 
             BossBar bossBar = Bukkit.createBossBar(new NamespacedKey(HubSystem.INSTANCE, "bossbar_" + player.getName()), "", BarColor.BLUE, BarStyle.SEGMENTED_6);
             bossBar.addPlayer(player);
